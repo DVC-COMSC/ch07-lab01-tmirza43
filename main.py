a8e47ee7c166e6606faac72062f12ca8a37b7a32
Talha Mirza <tmirza43@gmail.com>
2022-10-16 22:15:23 -0700

    commit1

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = []
+for i in range(5):
+    num = int(input("Enter num: "))
+    numbers.insert(i, num)
+
+numbers.sort()
+print(numbers[1] + numbers[2] + numbers[3])
